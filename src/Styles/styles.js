3742ee5d4ff9a3dc4ade14ac9de305b3fec00cc4
voidrizoma <milhadasdepapel@gmail.com>
2020-10-24 02:29:51 -0500

    readme

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,50 +1,53 @@
 import styled from "styled-components";
 
-export const Card = styled.div`    
-  display:flex;
-  flex-direction:column;
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color:#fbb311;
+  background-color: #fbb311;
   margin: 2%;
-  width:45%;
-  padding:3%;
-  heigth:30vh;
+  width: 40%;
+  padding: 3%;
+  heigth: 30vh;
   border-radius: 5px;
   transition: 0.3s;
-  &:hover{
+  &:hover {
     transform: translate(0, -10px);
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  @media screen and (max-width: 750px) {
+    margin: 2%;
+    width: 80%;
   }
 `;
 
 export const CardTitle = styled.h3`
   text-align: center;
-  font-size:20px;
-  padding:3% 0%;
+  font-size: 20px;
+  padding: 3% 0%;
 `;
 
 export const Span = styled.span`
   text-align: center;
-  font-size:18px;
+  font-size: 18px;
 `;
 
 export const Img = styled.img`
-  border-radius:50%;
+  border-radius: 50%;
   border: 5px solid #ddd;
 `;
 
-
 export const Button = styled.button`
-font-size: 15px;
-margin: 1em;
-padding: 0.25em 1em;
-border-radius: 3px;
-border: 2px solid  #fbb311;
-transition: 1s;
-&:hover{
+  font-size: 15px;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: 2px solid #fbb311;
+  transition: 1s;
+  &:hover {
     background-color: #fbb311;
-}
+  }
 `;
 
 export const CardWrapper = styled.div`
@@ -55,7 +58,6 @@ export const CardWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
 
 export const Wrapper = styled.div`
   height: 60vh;
@@ -70,21 +72,21 @@ export const Wrapper = styled.div`
 
 export const Title = styled.h1`
   text-align: center;
-  color:white;
-  font-size:50px;
-  padding:2% 0%;
+  color: white;
+  font-size: 50px;
+  padding: 2% 0%;
 `;
 
 export const Subtitle = styled.h1`
   text-align: center;
-  font-size:30px;
-  color:white;
-  padding:2% 0%;
+  font-size: 30px;
+  color: white;
+  padding: 2% 0%;
 `;
 
 export const Pages = styled.div`
   display: flex;
-  font-size:18px;
+  font-size: 18px;
   color: #00ca93;
   align-items: center;
   text-align: center;
@@ -95,7 +97,7 @@ export const ButtonContainer = styled.div`
   flex-direction: row;
   max-width: 1340px;
   margin: 0 auto;
-  margin-bottom:5%;
+  margin-bottom: 5%;
   justify-content: space-evenly;
 `;
 
